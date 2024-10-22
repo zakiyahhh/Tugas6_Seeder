791e54b80296d6b22afbccd9f1f1666710c526b4
sitizakiyah <sitizakiyah235@gmail.com>
2024-10-22 19:52:35 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function main() {
+  /**--------------- Not allowed to be edited - start - --------------------- */
+  const mongoUri = process.env.MONGODB_URI;
+  const collection = process.env.MONGODB_COLLECTION;
+
+  const args = process.argv.slice(2);
+
+  const command = args[0];
+  /**--------------- Not allowed to be edited - end - --------------------- */
+
+  // Connect to MongoDB
+  await mongoose.connect(mongoUri, {
+  });
+
+  // Define a schema for the collection
+  const schema = new mongoose.Schema({}, { strict: false });
+  const Model = mongoose.model(collection, schema);
+
+  switch (command) {
+    case "check-db-connection":
+      await checkConnection();
+      break;
+    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    default:
+      throw Error("command not found");
+  }
+
+  await mongoose.disconnect();
+  return;
+}
+
+async function checkConnection() {
+  console.log("check db connection started...");
+  try {
+    await mongoose.connection.db.admin().ping();
+    console.log("MongoDB connection is successful!");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+  console.log("check db connection ended...");
+}
+
+main();
